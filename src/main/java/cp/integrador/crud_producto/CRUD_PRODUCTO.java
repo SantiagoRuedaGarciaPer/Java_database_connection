363e85e831fd20b5cf5bae520ff01ede297bf4fc
Santiago Rueda Garcia <santirueda0917@gmail.com>
2026-07-17 06:41:38 -0500

    inicializacion repositorio

---
+++ src/main/java/cp/integrador/crud_producto/CRUD_PRODUCTO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cp.integrador.crud_producto;
+
+/**
+ *
+ * @author camper
+ */
+public class CRUD_PRODUCTO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
