37342fa9041a145fd43f16e0ac67a4e4cd0c250b
Santiago Rueda Garcia <santirueda0917@gmail.com>
2026-07-17 07:51:43 -0500

    crud categoria

---
+++ src/main/java/MODELO/CategoriaDAO.java
@@ -0,0 +1,84 @@
+package MODELO;
+
+import CONTROLADOR.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+
+public class CategoriaDAO {
+    Conexion c =  new Conexion();
+    JOptionPane jo = new JOptionPane();
+    
+    public void create(Categoria categoria){
+        try (Connection con=c.conectar()){
+            PreparedStatement ps = con.prepareStatement("INSERT INTO Categoria(nombre, descripcion) values (?, ?)");
+            ps.setString(1, categoria.getNombre());
+            ps.setString(2, categoria.getDescripcion());
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void update(Categoria categoria){
+        try (Connection con=c.conectar()){
+            PreparedStatement ps = con.prepareStatement("update Categoria set nombre=?, descripcion=? where id=?");
+            ps.setString(1, categoria.getNombre());
+            ps.setString(2, categoria.getDescripcion());
+            ps.setString(3, String.valueOf(categoria.getId()));
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    
+    public void delete(Categoria categoria){
+        int op = JOptionPane.showConfirmDialog(null, "Esta segur@ de eliminar a "+categoria.getNombre()+"?", null, JOptionPane.YES_NO_OPTION);
+        if(op == 0){
+            try (Connection con=c.conectar()){
+                PreparedStatement ps = con.prepareStatement("delete from Categoria where id=?");
+                ps.setInt(1, categoria.getId());
+                ps.executeUpdate();
+            } catch (SQLException e) {
+                System.out.println(e.getMessage());
+            }    
+        }else{
+            JOptionPane.showInternalMessageDialog(null, "Operacion cancelada");
+        }
+        
+    }
+    
+    public ArrayList<Categoria> listar(){
+        ArrayList<Categoria> result = new ArrayList<>();
+        try (Connection con=c.conectar()){
+            PreparedStatement ps = con.prepareStatement("select * from categoroia");
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                result.add(new Categoria(rs.getInt(1), rs.getString("nombre"), rs.getString("descripcion")));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+    
+    public Categoria buscar(int id){
+        Categoria result = null;
+        try (Connection con=c.conectar()){
+            PreparedStatement ps = con.prepareStatement("select * from categoroia where id =?");
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()){
+                result = new Categoria(rs.getInt("id"), rs.getString("nombre"), rs.getString("descripcion"));
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+        return result;
+    }
+    
+}
